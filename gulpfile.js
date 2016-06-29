ffce6e85e6452809fee536cef3459de2d2029d57
Takuya Takahashi <info@yujilow.net>
2016-06-29 09:42:50 +0900

    add tag sender

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var client = require('./lib/client');
 var gulp = require('gulp');
 var user = require('./user.json');
+var minimist = require('minimist');
+var argv = minimist(process.argv.slice(2));
+var tag = 'root';
 
 gulp.task('watch', function(){
 	gulp.watch('write-your-note.md', ['post']);
@@ -11,10 +14,15 @@ gulp.task('post', function(){
 		var post = {text:text, author:user.name, postedAt:Date.now()};
 		client.post(post, function(){
 			client.erase('./write-your-note.md', function(){
-				console.log("ok");
+				console.log("ok in " + tag);
 			});
 		});
 	});
 });
 
 gulp.task('default', ['watch']);
+
+gulp.task('tag', function() {
+  tag = argv.t || 'root';
+	gulp.watch('write-your-note.md',['post']);
+});
