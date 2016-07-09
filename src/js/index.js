8763bc9204bfa39d492b2589c7adce756f3a8ee7
Takuya Takahashi <info@yujilow.net>
2016-07-09 20:12:00 +0900

    add function of cache

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,56 @@ editor.setOptions({
 editor.getSession().setMode("ace/mode/markdown");
 editor.getSession().setUseWrapMode(true);
 editor.setFontSize(15);
+
+function noteClientViewModel(){
+  var self = this;
+
+  self.existTags = ko.observableArray([]);
+  self.tag = ko.observable("");
+  self.post = function(){
+    var content = editor.getValue();
+    var tag = self.tag();
+    var url = "http://localhost:3000/post";
+    var store = getStore();
+    var json = {
+      text: content,
+      tag: tag
+    }
+    saveJSON(store);
+    $.ajax({
+      type : 'post',
+      url : url,
+      data : JSON.stringify(json),
+      contentType: 'application/JSON',
+      dataType : 'JSON',
+      scriptCharset: 'utf-8',
+    }).done(function(data){
+    });
+  };
+
+  self.save = function(){
+    savePostToStore({aaa:'iii'});
+  }
+
+
+
+};
+
+
+ko.applyBindings(new noteClientViewModel());
+
+
+
+/**
+
+functions
+
+*/
+
+
+
+function saveJSON(json){
+  var store = $.parseJSON(localStorage.getItem('post_cache')) || [];
+  store.push(json);
+  localStorage.setItem('post_cache', JSON.stringify(store));
+}
