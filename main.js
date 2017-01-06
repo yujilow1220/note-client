f17c8b96e4d0d052e982ea4128de8da21d146636
Takuya Takahashi <info@yujilow.net>
2017-01-06 18:10:24 +0900

    mod path

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ app.on('window-all-closed', function() {
 });
 
 app.on('ready', function() {
-  pref.load('./preference.json');
+  pref.load(__dirname + '/preference.json');
   // ブラウザ(Chromium)の起動, 初期画面のロード
   mainWindow = new BrowserWindow({width: 1200, height: 600});
   mainWindow.loadURL('file://' + __dirname + '/src/index.html');
